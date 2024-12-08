b141bcb22063b6e23f3cf345da233bf73abf4431
Draculakemse <welcomechen22@gmail.com>
2024-12-08 17:48:19 +0000

    Update monekys.lua

--- monekys.lua
+++ monekys.lua
@@ -1,9 +1,9 @@
 getgenv().hub_key = "a9d94c557869d4b279b0e8af4a7d1ea8"
 getgenv().custom_label = "VPS1"
-getgenv().BabyFarmActive = true
-getgenv().MiniGameFarm = false
+getgenv().BabyFarmActive = false
+getgenv().MiniGameFarm = true
 ---------------------------------
-getgenv().PotFarm = true
+getgenv().PotFarm = false
 ---------------------------------
 getgenv().PetFarm = false
 getgenv().PetTypes = { "grave owl", "kraken", "cheetah" }
